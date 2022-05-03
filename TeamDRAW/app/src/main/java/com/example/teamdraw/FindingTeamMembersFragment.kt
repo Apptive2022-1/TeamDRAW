1c89f8a64a86964a26978f68a7881a3beb071540
Liv66 <rlawjdgjs73@gmail.com>
2022-05-04 01:08:09 +0900

    main_layout 초안
    * Contest, FindingTeamMember, SmallGroupRoom 프래그먼트와 레이아웃 생성
    * 메인 액티비티 xml에서 바텀 네비게이션 구현
    * 바텀네비게이션 메뉴 구현 ( 아이콘 변경해야 함)
    * Contest 프래그먼트 레이아웃에서 스피너, 리사이클러 뷰 구현
    * Contest_Detail 레이아웃 대략적으로 구현
    ---------------------------------
    해야할 일
    1. MainActivity 레이아웃 툴바 추가
    2. contest_detail에서 Coordinate_layout 구현
    3. contest_detail에서 Bottom_Sheet 구현
    4. finding_team, small_group 구현

---
+++ TeamDRAW/app/src/main/java/com/example/teamdraw/FindingTeamMembersFragment.kt
@@ -0,0 +1,18 @@
+package com.example.teamdraw
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FindingTeamMembersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_finding_team_members, container, false)
+    }
+}
