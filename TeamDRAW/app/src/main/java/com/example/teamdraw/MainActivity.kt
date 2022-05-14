705f8bb73eed8a0f9a9a70d29558660bbc1642a5
Liv66 <rlawjdgjs73@gmail.com>
2022-05-14 22:59:55 +0900

    fix: MainAcitivity
    setContentView 삭제

--- TeamDRAW/app/src/main/java/com/example/teamdraw/MainActivity.kt
+++ TeamDRAW/app/src/main/java/com/example/teamdraw/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         login() // 자동 로그인
 
